c90d627df0c694b6836579c10531cd28314de0da
Grantapher <granttpfr@gmail.com>
2022-09-10 16:19:10 -0700

    removing secrets

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-// todo move to secrets/ssm
-
-export default {
-  applicationId: "822651037990125608",
-  guildId: "602677492712603822",
-  token: "ODIyNjUxMDM3OTkwMTI1NjA4.GfwY6P.79IfnJI8CAvqtz2N1OvUhJqV7ZGbB15ryu9wrw",
-};
